ae0ec588a2ec1d678b0ba535a2536f7495ebdbac
Willis Walter <WillisWWalter@example.com>
2026-05-02 22:07:55 -0700

    Initial commit

--- src/components/map/AncientEgyptMap.tsx
+++ src/components/map/AncientEgyptMap.tsx
@@ -136,14 +136,6 @@ export default function AncientEgyptMap({ onSelectSite, selectedSite }: Props) {
           {markers}
         </MapContainer>
 
-        {/* 📱 MOBILE FILTER BUTTON - Hidden on desktop since filter is always visible */}
-        <button
-          onClick={() => setShowFilters(prev => !prev)}
-          className="absolute top-4 right-4 z-[2000] bg-black/70 text-white px-3 py-2 rounded-lg md:hidden"
-        >
-          Filters
-        </button>
-
         {/* DESKTOP HOVER TOOLTIP */}
         <AnimatePresence>
           {hoveredSite && (
